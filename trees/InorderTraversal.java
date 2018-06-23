88320111c588db31b7052b11d2a345d334048398
Pranav <pranavchauthaiwale@gmail.com>
2018-06-23 19:45:05 -0400

    =Iterative inorder and preorder traversal

--- trees/InorderTraversal.java
+++ trees/InorderTraversal.java
@@ -3,18 +3,42 @@ package trees;
 import trees.TreeUtils.TreeNode;
 import trees.TreeUtils.TreeUtils;
 
+import java.util.Stack;
+
 public class InorderTraversal {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         TreeNode root = TreeUtils.getBinaryTree();
-        InorderTraversal(root);
+        RecursiveInorderTraversal(root);
+        IterativeInorderTraversal();
     }
 
-    public static void InorderTraversal(TreeNode node){
-        if(node != null){
-            InorderTraversal(node.getLeftNode());
+    public static void RecursiveInorderTraversal(TreeNode node) {
+        if (node != null) {
+            RecursiveInorderTraversal(node.getLeftNode());
             System.out.println(node.getData());
-            InorderTraversal(node.getRightNode());
+            RecursiveInorderTraversal(node.getRightNode());
+        }
+    }
+
+    public static void IterativeInorderTraversal() {
+
+        TreeNode root = TreeUtils.getBinaryTree();
+        TreeNode currentNode = root;
+        Stack<TreeNode> stack = new Stack<>();
+        while (true) {
+            if (currentNode != null) {
+                stack.push(currentNode);
+                currentNode = currentNode.getLeftNode();
+            } else {
+                if (stack.isEmpty()) {
+                    break;
+                } else {
+                    currentNode = stack.pop();
+                    System.out.print(currentNode.getData() + " ");
+                    currentNode = currentNode.getRightNode();
+                }
+            }
         }
     }
 }
--- trees/PreorderTraversal.java
+++ trees/PreorderTraversal.java
@@ -3,18 +3,39 @@ package trees;
 import trees.TreeUtils.TreeNode;
 import trees.TreeUtils.TreeUtils;
 
+import java.util.Stack;
+
 public class PreorderTraversal {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         TreeNode root = TreeUtils.getBinaryTree();
-        preorderTraversal(root);
+        recursivePreorderTraversal(root);
+        iterativePreorderTraversal(root);
     }
 
-    public static void preorderTraversal(TreeNode node){
-        if(node != null){
+    public static void recursivePreorderTraversal(TreeNode node) {
+        if (node != null) {
             System.out.println(node.getData());
-            preorderTraversal(node.getLeftNode());
-            preorderTraversal(node.getRightNode());
+            recursivePreorderTraversal(node.getLeftNode());
+            recursivePreorderTraversal(node.getRightNode());
         }
     }
+
+    public static void iterativePreorderTraversal(TreeNode root) {
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode currentNode = stack.pop();
+            System.out.print(currentNode.getData() + " ");
+            if (currentNode.getRightNode() != null) {
+                stack.push(currentNode.getRightNode());
+            }
+            if (currentNode.getLeftNode() != null) {
+                stack.push(currentNode.getLeftNode());
+            }
+        }
+    }
+
 }
